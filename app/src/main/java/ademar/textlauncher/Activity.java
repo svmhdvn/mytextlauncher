8554e4b9b86d3d4ec7d860aada3a644f939df427
Siva Mahadevan <me@svmhdvn.name>
2025-01-14 13:22:53 -0500

    Siva's changes

--- app/src/main/java/ademar/textlauncher/Activity.java
+++ app/src/main/java/ademar/textlauncher/Activity.java
@@ -1,84 +1,81 @@
 package ademar.textlauncher;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 import static android.content.Intent.ACTION_MAIN;
 import static android.content.Intent.ACTION_PACKAGE_ADDED;
 import static android.content.Intent.ACTION_PACKAGE_REMOVED;
 import static android.content.Intent.ACTION_PACKAGE_REPLACED;
 import static android.content.Intent.CATEGORY_LAUNCHER;
-import static android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import name.svmhdvn.mytextlauncher.R;
 
 public final class Activity extends android.app.Activity implements
-        Comparator<Model>,
-        TextWatcher,
         AdapterView.OnItemClickListener,
-        AdapterView.OnItemLongClickListener,
-        TextView.OnEditorActionListener,
-        View.OnClickListener {
+        AdapterView.OnItemLongClickListener {
 
-    private final Adapter adapter = new Adapter();
+    private record Model(String label, String packageName) implements Comparable<Model> {
+        @Override
+        public int compareTo(Model other) {
+            return label.compareTo(other.label);
+        }
 
-    private EditText filter;
-    private ImageView clear;
-    private BroadcastReceiver broadcastReceiver;
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    private ArrayAdapter<Model> adapter;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            update();
+        }
+    };
+
+    private final Intent launcherIntent = new Intent(ACTION_MAIN, null).addCategory(CATEGORY_LAUNCHER);
+    private final Intent appDetailsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, null).addCategory(CATEGORY_LAUNCHER);
+
+    public Activity() {
+        super();
+        intentFilter.addAction(ACTION_PACKAGE_ADDED);
+        intentFilter.addAction(ACTION_PACKAGE_REMOVED);
+        intentFilter.addAction(ACTION_PACKAGE_REPLACED);
+        intentFilter.addDataScheme("package");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity);
 
+        adapter = new ArrayAdapter<Model>(this, R.layout.model);
+
         update();
 
-        ListView list = findViewById(R.id.list);
-        filter = findViewById(R.id.filter);
-        clear = findViewById(R.id.clear_filter);
-
+        final ListView list = findViewById(R.id.list);
         list.setAdapter(adapter);
         list.setOnItemClickListener(this);
         list.setOnItemLongClickListener(this);
-
-        filter.addTextChangedListener(this);
-        filter.setOnEditorActionListener(this);
-        clear.setOnClickListener(this);
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(ACTION_PACKAGE_ADDED);
-        intentFilter.addAction(ACTION_PACKAGE_REMOVED);
-        intentFilter.addAction(ACTION_PACKAGE_REPLACED);
-        intentFilter.addDataScheme("package");
-
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                update();
-            }
-        };
-
         registerReceiver(broadcastReceiver, intentFilter);
     }
 
@@ -89,40 +86,9 @@ public final class Activity extends android.app.Activity implements
     }
 
     @Override
-    public void onBackPressed() {
-    }
-
-    @Override
-    public int compare(Model lhs, Model rhs) {
-        return lhs.label.compareToIgnoreCase(rhs.label);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-    }
-
-    @Override
-    public void afterTextChanged(Editable editable) {
-        adapter.filter(editable.toString());
-        if (clear != null) {
-            clear.setVisibility(editable.length() > 0 ? View.VISIBLE : View.GONE);
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-        clearSearch();
-    }
-
-    @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int index, long id) {
-        hideKeyboard();
         try {
-            startActivity(getPackageManager().getLaunchIntentForPackage(adapter.getItem(index).packageName));
+            startActivity(getPackageManager().getLaunchIntentForPackage(Objects.requireNonNull(adapter.getItem(index)).packageName));
         } catch (Exception e) {
             Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
         }
@@ -130,58 +96,23 @@ public final class Activity extends android.app.Activity implements
 
     @Override
     public boolean onItemLongClick(AdapterView<?> adapterView, View view, int index, long id) {
-        hideKeyboard();
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        intent.setData(Uri.fromParts("package", adapter.getItem(index).packageName, null));
         try {
-            startActivity(intent);
+            startActivity(appDetailsIntent.setData(Uri.fromParts("package", Objects.requireNonNull(adapter.getItem(index)).packageName, null)));
         } catch (Exception e) {
-            Toast.makeText(this, adapter.getItem(index).packageName, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
         }
         return true;
     }
 
-    @Override
-    public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-        return actionId == IME_ACTION_SEARCH && hideKeyboard();
-    }
-
-    private boolean hideKeyboard() {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        if (inputMethodManager != null && filter != null) {
-            inputMethodManager.hideSoftInputFromWindow(filter.getWindowToken(), 0);
-            return true;
-        }
-        return false;
-    }
-
-    private void clearSearch() {
-        if (filter != null) {
-            filter.setText("");
-        }
-        hideKeyboard();
-    }
-
     private void update() {
-        clearSearch();
-
-        PackageManager packageManager = getPackageManager();
-        Intent intent = new Intent(ACTION_MAIN, null);
-        intent.addCategory(CATEGORY_LAUNCHER);
-        List<ResolveInfo> availableActivities = packageManager.queryIntentActivities(intent, 0);
-        ArrayList<Model> models = new ArrayList<>();
-        long id = 0;
-        for (ResolveInfo resolveInfo : availableActivities) {
-            if ("ademar.textlauncher".equalsIgnoreCase(resolveInfo.activityInfo.packageName)) continue;
-            models.add(new Model(
-                    ++id,
-                    resolveInfo.loadLabel(packageManager).toString(),
-                    resolveInfo.activityInfo.packageName
-            ));
-        }
-        Collections.sort(models, this);
-        adapter.update(models);
+        final PackageManager packageManager = getPackageManager();
+        adapter.clear();
+        adapter.addAll(packageManager.queryIntentActivities(launcherIntent, 0)
+                .stream()
+                .map(r -> new Model(r.loadLabel(packageManager).toString(), r.activityInfo.packageName))
+                .sorted()
+                .collect(Collectors.toList()));
+        adapter.notifyDataSetChanged();
     }
 
 }
